c5636c60dfce8aef539b52e671edae06f3eb1ff6
Evan Stoddard <evan.stoddard@ovyl.io>
2025-08-15 15:44:47 -0500

    Updated toolchain shell script.

--- scripts/toolchain_shell.sh
+++ scripts/toolchain_shell.sh
@@ -5,11 +5,8 @@ g_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 
 # Prevents this script from being run directly
 if [ ${0##*/} == ${BASH_SOURCE[0]##*/} ]; then 
-    echo "WARNING"
-    echo "This script is not meant to be executed directly!"
-    echo "Use this script only by sourcing it."
-    echo
-    exit 1
+    bash --rcfile <(echo ". \"$g_SCRIPT_DIR/toolchain_shell.sh\"; exec bash -i")
+    exit 0
 fi
 
 # Source common_config.sh for common variables
