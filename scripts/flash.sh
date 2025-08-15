4352de788349a2091f58f82e4df28de2f451af8f
Evan Stoddard <evan.stoddard@ovyl.io>
2025-08-15 16:42:51 -0500

    Fixed build script and added flash script.

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -6,11 +6,6 @@ g_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 # Source common.sh for common variables
 source ${g_SCRIPT_DIR}/common.sh
 
-# Ensure project initialized
-if [ ! -d ${g_ROOT_DIR}/.west ]; then
-    echo "Project doesn't exist..."
-    ${g_SCRIPT_DIR}/init_project.sh
-fi
 
 # Build default target with default board
-west build --board=${g_DEFAULT_BUILD_BOARD} ${g_PROJECT_DIR}/${g_DEFAULT_BUILD_TARGET} $@
+west flash -d build $@
